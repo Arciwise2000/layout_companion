15f235d6ddddc1714add249a57003c5792313124
Ami <arciwis3@gmail.com>
2025-07-27 18:56:59 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 
 bl_info = {
     "name": "Layout Companion",
-    "version": (1, 5, 6),
+    "version": (1, 5, 7),
     "author": "Arciwise",
     "blender": (4, 5, 0),
     "description": "Tools for Layouters",

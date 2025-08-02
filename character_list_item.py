36774ddb8d21fd2a1d4851f2e83294a8c96a2369
Ami <arciwis3@gmail.com>
2025-08-02 03:20:16 -0400

    Delete character_list_item.py

--- character_list_item.py
+++
@@ -1,4 +0,0 @@
-import bpy
-class CharacterListItem(bpy.types.PropertyGroup):
-    name: bpy.props.StringProperty(name="name")  # type: ignore
-    scale: bpy.props.FloatProperty(name="scale")  # type: ignore
